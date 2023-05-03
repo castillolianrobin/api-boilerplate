dde512cf665a031cb2a44ee9eb28b24035cb3430
Lian Robin Castillo <lian@thinkbitsolutions.com>
2023-05-03 22:21:42 +0800

    Chore: Add test route

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,13 @@ import express from 'express';
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   
+   app.get("/test", (req, res) =>
+    res.status(200).send({ 
+      message: "Showing Test Page",
+    })
+  );
+
+  
   //default catch-all route that sends a JSON response.
   app.get("*", (req, res) =>
     res.status(401).send({ 
