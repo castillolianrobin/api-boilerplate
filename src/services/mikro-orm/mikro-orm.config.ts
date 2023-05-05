c9ecdbd69a6041c3fd09f07c42499a8571799a9c
Lian Robin Castillo <lian@thinkbitsolutions.com>
2023-05-05 20:16:13 +0800

    Fix: Incorrect migration in mikro orm

---
+++ src/services/mikro-orm/mikro-orm.config.ts
@@ -0,0 +1,21 @@
+import { Options, UnderscoreNamingStrategy } from '@mikro-orm/core';
+import { MikroORM } from '@mikro-orm/core';
+
+const config: Options = {
+  entities: ["dist/**/entities/**/*.js"],
+  entitiesTs: ["src/**/entities/**/*.ts"],
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || ''),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  dbName: process.env.DB_NAME,
+  debug: process.env.NODE_ENV !== 'production',
+  migrations: {
+    path: './dist/mikro-orm.config.js',
+    pathTs: './src/mikro-orm.config.ts',
+  },
+  tsNode: process.env.NODE_ENV !== 'production',
+};
+
+export default config;
